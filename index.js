0793b9ff95a9ad88696b97f7f59413977f5669c6
Edison Jeon <edison-jeon@lambdastudents.com>
2021-02-24 20:00:40 -0800

    was assisted through this project, i believe i'm having serious issues with understanding the fundamentals of functions and values, reviewing to catch up.

--- index.js
+++ index.js
@@ -39,8 +39,13 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+    this.toString = function(){
+      return `${this.name}, ${this.age}`
+    }
   }
  
  
@@ -63,9 +68,21 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
+
+  Car.prototype.fill = function (gallons) {
+    this.tank += gallons;
+  }
+
+  const vehicle = new Car('Subaru', 22);
+console.log(vehicle);
+
+  
   
   
   /*
@@ -75,18 +92,24 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age)
+   this.favoriteToy = favoriteToy;
   }
  
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function () {
+    return `Playing with ${this.favoriteToy}.`
+  }
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. It calls back an object
+    2. It acts as an universal callback
+    3. it operates in a global scope
+    4. ???
   */
   
   
